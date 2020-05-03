7bf49326bbf15ffe3285432d8d6da9ee13f0ade4
Luccas Avila <hey@luccasavila.dev>
2020-05-03 14:21:21 -0300

    Added normalizePort to server

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-server.listen(3000, () => {
-    console.log('Project running on port', server.address().port);
+const port = normalizePort(process.env.PORT || 3000);
+server.listen(port, () => {
+  console.log('Project running on port', server.address().port);
 })
 
 app.use(express.static('client'));
@@ -12,7 +13,19 @@ app.use(express.static('client'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/index.html');
 });
-
 app.get('/single', (req, res) => {
   res.sendFile(__dirname + '/client/game.html');
 });
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
