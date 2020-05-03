df0c2fdd026f1ec11fe45d79a8f82e26747fecb9
Luccas Avila <luccas@pop-os.localdomain>
2020-05-02 21:39:53 -0300

    Created initial structure and server of project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(3000, () => {
+    console.log('Project running on port', server.address().port);
+})
+
+app.use(express.static('client'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/index.html');
+});
