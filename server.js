71110d48eb5f30305e635bf75c4b1244aea9f021
Luccas Avila <hey@luccasavila.dev>
2020-05-04 11:34:00 -0300

    Starting to create the multiplayer mode

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const room = require('./room')
 
 const port = normalizePort(process.env.PORT || 3000);
 server.listen(port, () => {
@@ -9,11 +10,17 @@ server.listen(port, () => {
 })
 
 app.use(express.static('client'));
+//set socketio on express to use globally
+app.set('socketio', io);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/index.html');
 });
 app.get('/single', (req, res) => {
+  res.sendFile(__dirname + '/client/game.html');
+});
+app.get('/room', (req, res) => {
+  room(req.app.get('socketio'))
   res.sendFile(__dirname + '/client/game.html');
 });
 
